06a01d67f9ae7d8a8ed4d3b2323ae9999c3d224a
涂佳涵 <tujiahan@tujiahandembp.lan>
2022-05-08 17:59:29 -0700

    update deposit

---
+++ src/main/java/com/example/bank/controller/DepositController.java
@@ -0,0 +1,24 @@
+package com.example.bank.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+public class DepositController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
+    @RequestMapping("/deposit")
+    public boolean deposit(String username, double money) {
+        if(money<0){
+            System.out.println("Please enter the valid money");
+            return false;
+        }
+        Double deposit = Double.valueOf(String.valueOf(jdbcTemplate.queryForMap("select balance from deposit where username=?", username)));
+        jdbcTemplate.update("update deposit set balance=? where username=?", money+deposit, username);
+        return true;
+    }
+}
