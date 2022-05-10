17366263f551a5ca300f93a5ceaf0790ea782107
Victoria Niu <victoriacoo98@yahoo.com>
2022-05-09 23:43:34 -0700

    Update amount to double and added regex

--- src/main/java/com/swe266/bank/service/BankImpl/TransferServiceImpl.java
+++ src/main/java/com/swe266/bank/service/BankImpl/TransferServiceImpl.java
@@ -14,22 +14,29 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @Service("TransferService")
 public class TransferServiceImpl implements TransferServiceI {
     @Autowired
     private JdbcTemplate jdbcTemplate;
     private Logger logger = LoggerFactory.getLogger(LoginServiceImpl.class);
+    private static final Pattern p = Pattern.compile("^(([1-9]{1}\\d*)|(0{1}))(\\.\\d{1,2})?$");
     @Override
     public boolean transfer(String username, String transfer_amount, String thisUsername) {
         logger.info(username,thisUsername,transfer_amount);
         logger.info("select deposit from user where username='"+username+"'");
-        int money = Integer.parseInt(transfer_amount);
+
+        if (! p.matcher(transfer_amount).matches()){
+            logger.info("Invalid amount!");
+            return false;
+        }
+        double money = Integer.valueOf(transfer_amount);
         Map<String, Object> resultMap = jdbcTemplate.queryForMap("select deposit from user where username='"+username+"'");
 
-        int toBalance = (int) resultMap.get("deposit") + money;
+        double toBalance = (int) resultMap.get("deposit") + money;
         Map<String, Object> resultMap1 = jdbcTemplate.queryForMap("select deposit from user where username='"+thisUsername+"'");
-        int myBalance = (int) resultMap1.get("deposit") - money;
+        double myBalance = (int) resultMap1.get("deposit") - money;
         if (myBalance<0){
             System.out.println("Not enough money to transfer!");
             return false;
